7c50f3ec0ad35fec42b55134e74718000a9122ed
LivaZet <57807423+LivaZet@users.noreply.github.com>
2020-04-01 23:17:40 +0300

    DB fix attempt

--- src/main/java/lv/accenture/bootcamp/rardb/controller/IndexController.java
+++ src/main/java/lv/accenture/bootcamp/rardb/controller/IndexController.java
@@ -3,19 +3,27 @@ package lv.accenture.bootcamp.rardb.controller;
 
 import lv.accenture.bootcamp.rardb.model.Review;
 import lv.accenture.bootcamp.rardb.repository.ReviewRepository;
+import lv.accenture.bootcamp.rardb.service.TopReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.sql.SQLException;
 import java.util.Collections;
 import java.util.List;
+
 
 @Controller
 public class IndexController {
 
+
     @Autowired
     private ReviewRepository reviewRepository;
+
 
     @GetMapping("/")
     public ModelAndView toIndex() {
@@ -31,4 +39,17 @@ public class IndexController {
         return modelAndView;
     }
 
-}
+//    @GetMapping ("/")
+//    public String toIndex(Model model) {
+//        try {
+//            model.addAttribute("review", TopReviewService.topReviews());
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return "/";
+//    }
+
+
+    }
+
+
